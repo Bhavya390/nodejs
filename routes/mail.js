6205218b3ee82ad985fb9583302c40e2e3092f53
bhavp <bhavp@paypal.com>
2018-09-22 19:55:40 +0530

    defined the separate functions for different functionalities in mail.js

--- routes/mail.js
+++ routes/mail.js
@@ -7,15 +7,14 @@ var ArrayList = require('ArrayList');
 db.connect();
 
 /* GET /mail */
-router.get('/', async function(req, res, next) {
-  let parms = { title: 'Inbox', active: { inbox: true } };
+router.get('/sendBirthdayWishes', async function(req, res, next) {
+  let parms = { title: 'SendBirthdayWishes', active: { sendBirthdayWishes: true } };
 
   const accessToken = await authHelper.getAccessToken(req.cookies, res);
   const userName = req.cookies.graph_user_name;
 
   if (accessToken && userName) {
     parms.user = userName;
-
     // Initialize Graph client
     const client = graph.Client.init({
       authProvider: (done) => {
@@ -23,50 +22,113 @@ router.get('/', async function(req, res, next) {
       }
     });
 
-  db.query("select * from employeeDay where DATE_FORMAT(DOB,'%m/%d') = DATE_FORMAT(CURDATE(),'%m/%d')", function (error, results, fields) {
-    if (error) throw error;
-
-    if(results != null) {
-      var employeeSameDOB = new ArrayList;
+    var employeeSameDOB = new ArrayList;
+    
+    db.query("select * from employeeDay where DATE_FORMAT(DOB,'%m/%d') = DATE_FORMAT(CURDATE(),'%m/%d')", function (error, results, fields) {
+      if (error) throw error;
       results.forEach(function(employee) {
-        employeeSameDOB.add(employee['EmpName'] + " from " +employee['Org'] + " Team" + "\n");
+        employeeSameDOB.push(employee['EmpName'] + " from " +employee['Org'] + " Team" + "</br>");
       });
-    // construct the email object
-    const mail = {
-      subject: "Microsoft Graph JavaScript Sample",
-      toRecipients: [{
-        emailAddress: {
+      if(employeeSameDOB.length > 0) {
+        // construct the email object
+        const mail = {
+          subject: "Microsoft Graph JavaScript Sample",
+          toRecipients: [{
+            emailAddress: {
             address: "bhavp@paypal.com"
+          }
+        }],
+          body: {
+            content: "<h1>Wishing you all very happy birthday from paypal </h1></br><p>" + employeeSameDOB,
+            contentType: "html"
+          }
         }
-      }],
-      body: {
-        content: "<h1>see how many ppl have same DOB " + "\n" + employeeSameDOB,
-        contentType: "html"
-      }
-    }
  
-    try {
-      client
-        .api('/users/me/sendMail')
-        .post({message: mail}, (err, res) => {
-        console.log(res)
-      });
-    } catch (err) {
-      parms.message = 'Error in sending messages';
-      parms.error = { status: `${err.code}: ${err.message}` };
-      parms.debug = JSON.stringify(err.body, null, 2);
-      res.render('error', parms);
-    } 
-  } 
-    //res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-  });
-
+        try {
+          client
+            .api('/users/me/sendMail')
+            .post({message: mail}, (err, res) => {
+            console.log(res)
+          });
+        } catch (err) {
+          parms.message = 'Error in sending messages';
+          parms.error = { status: `${err.code}: ${err.message}` };
+          parms.debug = JSON.stringify(err.body, null, 2);
+          res.render('error', parms);
+        } 
+      }
+    });
+    //} 
   } else {
     // Redirect to home
     res.redirect('/');
   }
 
-  res.render('index', parms);
+  res.render('SucessPage', parms);
+});
+
+/* GET /mail */
+router.get('/sendWorkingAnniversaryWishes', async function(req, res, next) {
+  let parms = { title: 'SendWorkingAnniversaryWishes', active: { sendWorkingAnniversaryWishes: true } };
+
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userName = req.cookies.graph_user_name;
+
+  if (accessToken && userName) {
+    parms.user = userName;
+    // Initialize Graph client
+    const client = graph.Client.init({
+      authProvider: (done) => {
+        done(null, accessToken);
+      }
+    });
+
+    var employeeSameDOJ = new ArrayList;
+    
+    db.query("select * from employeeDay where DATE_FORMAT(DOJ,'%m/%d') = DATE_FORMAT(CURDATE(),'%m/%d')", function (error, results, fields) {
+      if (error) throw error;
+      results.forEach(function(employee){
+        var DOJ = new Date(employee['DOJ']);
+        var currentDate = new Date();
+        var noOfWorkingMonths = currentDate.getYear() - DOJ.getYear();
+        if(noOfWorkingMonths%1 == 0) {
+          employeeSameDOJ.push(employee['EmpName'] + " from "+ employee['Org'] + " Team" + " Congratulations on your Service Anniversary at PayPal! for " + noOfWorkingMonths +" years looking for many more years to come :) ATB");
+        }
+      });
+      if(employeeSameDOJ.length > 0) {
+        // construct the email object
+        const mail = {
+          subject: "Microsoft Graph JavaScript Sample",
+          toRecipients: [{
+            emailAddress: {
+            address: "bhavp@paypal.com"
+          }
+        }],
+          body: {
+            content: "<h1>Congratulations to all for your annual Service Anniversary at PayPal, looking for many more years to come :) ATB</h1></br>" + employeeSameDOJ,
+            contentType: "html"
+          }
+        }
+ 
+        try {
+          client
+            .api('/users/me/sendMail')
+            .post({message: mail}, (err, res) => {
+            console.log(res)
+          });
+        } catch (err) {
+          parms.message = 'Error in sending messages';
+          parms.error = { status: `${err.code}: ${err.message}` };
+          parms.debug = JSON.stringify(err.body, null, 2);
+          res.render('error', parms);
+        } 
+      }
+    });
+  } else {
+    // Redirect to home
+    res.redirect('/');
+  }
+  res.render('SucessPage', parms);
 });
 
 module.exports = router;
